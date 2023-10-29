2ce480be685765230ad0de2fe043dffde7e9b45a
Jonas <jse@ipoox-group.com>
2023-10-29 10:49:17 +0000

    init api project

---
+++ config/config.exs
@@ -0,0 +1,44 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+import Config
+
+config :teams,
+  ecto_repos: [Teams.Repo],
+  generators: [timestamp_type: :utc_datetime]
+
+# Configures the endpoint
+config :teams, TeamsWeb.Endpoint,
+  url: [host: "localhost"],
+  adapter: Phoenix.Endpoint.Cowboy2Adapter,
+  render_errors: [
+    formats: [json: TeamsWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: Teams.PubSub,
+  live_view: [signing_salt: "1AQJiXmq"]
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :teams, Teams.Mailer, adapter: Swoosh.Adapters.Local
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
